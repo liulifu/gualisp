5db840742f4d82f002cd9f2f8a75f466a57ec5f4
liulifu <lifu2015@users.noreply.github.com>
2026-05-23 22:34:05 +0800

    Remove local Bun path from scripts

--- gua.cmd
+++ gua.cmd
@@ -1,7 +1,6 @@
 @echo off
 setlocal
 
-set "BUN_EXE=D:\Githubresource\yueke\bun\normal\bun-windows-x64\bun.exe"
-if not exist "%BUN_EXE%" set "BUN_EXE=bun"
+if "%BUN_EXE%"=="" set "BUN_EXE=bun"
 
 "%BUN_EXE%" "%~dp0bin\gua.mjs" %*
